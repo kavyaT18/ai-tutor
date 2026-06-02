b515542973270fabc2db5eaf0e0fd5cc8d895f20
Kaavya Satish Tawade <kavya18@Kaavyas-MacBook-Air.local>
2026-06-02 23:35:58 +0530

    Expose API globally

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,7 @@
 const API =
   window.location.hostname === "localhost"
     ? "http://localhost:8000"
-    : "https://ai-tutor-1-9ypl.onrender.com"
+    : "https://ai-tutor-1-9ypl.onrender.com";
 
+window.API = API;
     
