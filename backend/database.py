de85c167d5026e2bea96133235b16a16c0491f26
Kaavya Satish Tawade <kavya18@Kaavyas-MacBook-Air.local>
2026-06-02 15:48:53 +0530

    final final

--- backend/database.py
+++ backend/database.py
@@ -7,11 +7,9 @@ from sqlalchemy import text
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-print("DATABASE_URL =", DATABASE_URL)
+
 engine = create_engine(DATABASE_URL)
-with engine.connect() as conn:
-    result = conn.execute(text("SELECT current_user"))
-    print("POSTGRES USER:", result.scalar())
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
